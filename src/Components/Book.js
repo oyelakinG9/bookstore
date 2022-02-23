6a71b1541abb0e62fef7d7a60b3250b529fcbca5
oyelakinG9 <oyelakinr37@gmail.com>
2022-02-23 21:29:46 +0100

    Modify book js file

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/Books/Books';
 
 const Book = ({ bookInfo }) => {
-  const {
-    title, author,
-  } = bookInfo;
+  const dispatch = useDispatch();
+  const { id, title, author } = bookInfo;
   return (
-    <div>
-      <h3>{title}</h3>
-      <p>{author}</p>
+    <div className="card-container">
+      <div className="card">
+        <div className="card-itme">
+          <p className="category">Action, Adventure, Comedy</p>
+          <h3>{title}</h3>
+          <p className="author">{author}</p>
+          <div className="book-options">
+            <button onClick={() => dispatch(removeBook(id))} type="button">Remove</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
