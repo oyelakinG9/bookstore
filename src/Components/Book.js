d9eb995ba1f2aa493d4e84ea27454f08cbc9eb41
oyelakinG9 <oyelakinr37@gmail.com>
2022-02-24 18:53:15 +0100

    Modify book store

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -4,14 +4,14 @@ import { removeBook } from '../redux/Books/Books';
 
 const Book = ({ bookInfo }) => {
   const dispatch = useDispatch();
-  const { id, title, author } = bookInfo;
+  const { item_id: id, title, category } = bookInfo;
   return (
     <div className="card-container">
       <div className="card">
         <div className="card-itme">
           <p className="category">Action, Adventure, Comedy</p>
           <h3>{title}</h3>
-          <p className="author">{author}</p>
+          <p className="author">{category}</p>
           <div className="book-options">
             <button onClick={() => dispatch(removeBook(id))} type="button">Remove</button>
           </div>
